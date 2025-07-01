4eac59cfc0f082122e5989f3c4d1e4b8e9c3b4e7
AsandaMnisi <asandakia3@gmail.com>
2025-07-01 17:33:19 +0200

    Update DetailsStorage.java

--- DetailsStorage.java
+++ DetailsStorage.java
@@ -5,4 +5,5 @@ public class DetailsStorage {
     public static ArrayList <Tasks> details = new ArrayList<>();
     public  static   ArrayList<Character> priority = new ArrayList<Character>();
     public static ArrayList <Tasks> completedTasks = new ArrayList<>();
-}
+    public  static ArrayList <Tasks> finishedTasks = new ArrayList<>();
+ }
