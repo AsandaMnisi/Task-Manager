f2c480ef49464739772dc18152dfbd71ca3efdd6
Asanda Mnisi <asandakia3@gmail.com>
2025-07-02 17:02:06 +0200

    Update DetailsStorage.java

--- DetailsStorage.java
+++ DetailsStorage.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class DetailsStorage {
-    public static ArrayList <Tasks> details = new ArrayList<>();
-    public  static   ArrayList<Character> priority = new ArrayList<Character>();
     public static ArrayList <Tasks> completedTasks = new ArrayList<>();
     public  static ArrayList <Tasks> finishedTasks = new ArrayList<>();
  }
